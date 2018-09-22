a284efe7f8fd13530d04c059f61f81dc48ac307d
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-22 20:17:24 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -4,21 +4,4 @@ end
   
 def array_with_two_elements
   two_elements = ["fire", "water"]
-  
-def first_element
-  
-  
-def third_element
-  
-  
-def last_element
-  
-  
-def first_element_with_array_methods
-
-
-def last_element_with_array_methods
-
-
-def length_of_array
-
+end
