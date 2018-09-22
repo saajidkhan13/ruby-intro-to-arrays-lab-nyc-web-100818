00a7599d54c0517f5ab8d925693636efb6eba783
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-22 20:14:52 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,23 @@
+def instantiate_new_array
+  
+  
+def array_with_two_elements
+  
+  
+def first_element
+  
+  
+def third_element
+  
+  
+def last_element
+  
+  
+def first_element_with_array_methods
+
+
+def last_element_with_array_methods
+
+
+def length_of_array
+
